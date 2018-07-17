794fcd9503fcd944aea59aa10ee0832d2779bf8d
Jacob <jseiler@swin.edu.au>
2018-07-17 16:34:42 +1000

    Added more tests.
    This includes unit tests in which we compare the data type of the output grid
    and the shape of the output grid.
    
    Have also added a random test in which we generate a grid of random values from
    a known seed.  This is then compared against an npz file.

--- tests/tests.py
+++ tests/tests.py
@@ -19,13 +19,64 @@ sys.path.append(script_dir)
 import subsample
 
 
+def unit_tests(grid, expected_gridsize, expected_datatype=np.float64):
+    """
+    Tests some fundamental properties of the grids output by `subsample`.
+
+    Parameters
+    ----------
+
+    grid : `~numpy.ndarray`
+        The 3D downsampled array created by `subsample`. 
+    
+    expected_gridsize : int, optional
+        The expected 1D size of output grid.  Default : 128 and 64.
+
+    Returns
+    ----------
+
+    None.
+
+    Errors
+    ----------
+
+    RuntimeError
+        Raised if the output grid is not in the expected shape specified when
+        `subample` was called.
+        Raised if any values in the output grid are not `np.float64` types. 
+    """
+
+    # Check that the grid is cubic with the expected gridsize.
+    if not grid.shape == (expected_gridsize, 
+                          expected_gridsize,
+                          expected_gridsize):
+        print("The output grid shape was expected to be ({0}, {0}, {0})" \
+              .format(expected_gridsize))
+        print("However the shape was {0}".format(grid.shape))
+        raise RuntimeError
+
+    # Check that every element is of the expected datatype.
+    for (i, j, k) in itertools.product(range(expected_gridsize),
+                                       range(expected_gridsize),
+                                       range(expected_gridsize)):
+        if not type(grid[i,j,k]) == expected_datatype: 
+            print("The input data format was a `{4}`.  The output " 
+                  "data type should be identical however for element " 
+                  "({0},{1},{2}) it was {3}".format(i,j,k,type(grid[i,j,k]),
+                    expected_datatype)) 
+            raise RuntimeError
+
+
 def test_homogenous_input(input_gridsize=128, output_gridsize=64):
     """
     Tests that passing a homogenous input grid will produce a homogenous
     output.
 
     The test will fail if the output grid is not homogenously filled with
-    values of 1.0 or the output grid is not of the expected shape.
+    values of 1.0.
+
+    We also run a suite of unit tests (see `unit_tests` function) that will
+    return a RuntimeError if they're not passed.
 
     Parameters
     ----------
@@ -49,12 +100,11 @@ def test_homogenous_input(input_gridsize=128, output_gridsize=64):
     RuntimeError
         Raised if the output grid contains values that are not within the range
         0.99999 to 1.00001.
-        Raised if the output grid is not of the shape (`output_gridsize`,
-        `output_gridsize`, `output_gridsize`).
     """
 
-    input_grid = np.ones((input_gridsize, input_gridsize, input_gridsize))
-    output_grid = subsample.subsample_grid(input_grid, output_gridsize)    
+    input_grid = np.ones((input_gridsize, input_gridsize, input_gridsize),
+                         dtype=np.float64)
+    output_grid = subsample.subsample_grid(input_grid, output_gridsize)
 
     w = np.where((output_grid <= 0.99999) & (output_grid >= 1.00001))[0]
 
@@ -65,14 +115,9 @@ def test_homogenous_input(input_gridsize=128, output_gridsize=64):
         print("However cells {0} had values {1}".format(w, output_grid[w]))
         raise RuntimeError
 
-    if not output_grid.shape == (output_gridsize, 
-                                 output_gridsize,
-                                 output_gridsize):
-        print("The output grid shape was expected to be ({0}, {0}, {0})" \
-              .format(output_gridsize))
-        print("However the shape was {0}".format(output_grid.shape))
-        raise RuntimeError
-        
+    # Now run some unit tests that check some properties.
+    unit_tests(output_grid, output_gridsize)
+
 
 def test_multiple_input(input_gridsize=128, output_gridsize=64):
     """
@@ -81,7 +126,10 @@ def test_multiple_input(input_gridsize=128, output_gridsize=64):
     a grid that is homogenously filled with values of 1.0. 
 
     The test will fail if the output grid is not homogenously filled with
-    values of 1.0 or the output grid is not of the expected shape.
+    values of 1.0. 
+
+    We also run a suite of unit tests (see `unit_tests` function) that will
+    return a RuntimeError if they're not passed.
 
     Parameters
     ----------
@@ -105,8 +153,6 @@ def test_multiple_input(input_gridsize=128, output_gridsize=64):
     RuntimeError
         Raised if the output grid contains values that are not within the range
         0.99999 to 1.00001.
-        Raised if the output grid is not of the shape (`output_gridsize`,
-        `output_gridsize`, `output_gridsize`).
     """
 
     conversion = int(input_gridsize / output_gridsize)
@@ -129,13 +175,71 @@ def test_multiple_input(input_gridsize=128, output_gridsize=64):
         print("However cells {0} had values {1}".format(w, output_grid[w]))
         raise RuntimeError
 
-    if not output_grid.shape == (output_gridsize, 
-                                 output_gridsize,
-                                 output_gridsize):
-        print("The output grid shape was expected to be ({0}, {0}, {0})" \
-              .format(output_gridsize))
-        print("However the shape was {0}".format(output_grid.shape))
+    # Now run some unit tests that check some properties.
+    unit_tests(output_grid, output_gridsize)
+
+
+
+def test_random(save_output=False):
+    """
+
+    Parameters
+    ----------
+
+        ..note::
+            `output_gridsize` must be an integer multiple (and smaller) than
+            `input_gridsize`.  If not, a `RuntimeError` will be raised by
+            `subsample.subsample_grid`.
+
+    Returns
+    ----------
+
+    None.
+
+    Errors
+    ----------
+
+    RuntimeError
+        Raised if the output grid contains values that are not within the range
+        0.99999 to 1.00001.
+    """
+
+    # These parameters have been hardcoded to match the specs of the known grid
+    # we're testing against.
+    # If you want to test different parameters, set `save_output` to True.
+    input_gridsize=128
+    output_gridsize=64
+    seed=12
+
+    np.random.seed(seed)
+
+    input_grid = np.random.rand(input_gridsize,
+                                input_gridsize,
+                                input_gridsize)
+
+    output_grid = subsample.subsample_grid(input_grid, output_gridsize)
+
+    known_grid_name = "{0}/known_grid_in{1}_out{2}_seed{3}.npz" \
+                      .format(test_dir, input_gridsize, output_gridsize, seed)
+    if save_output: 
+        np.savez(known_grid_name, output_grid)
+
+    known_grid = (np.load(known_grid_name))["arr_0"]
+    known_grid.shape = (output_gridsize, output_gridsize, output_gridsize)
+
+    w = np.where(abs(known_grid - output_grid) > 1e-6)[0]
+
+    if len(w) > 0:
+        print("We compared an input grid with randomly generated data with "
+              "random seed {0}.  Reading the known input grid ({1}), we had "
+              "cells that contained different values.".format(seed,
+              known_grid_name))
+        print("These were cells {0} with difference {1}".format(w,
+              known_grid-output_grid[w]))
         raise RuntimeError
+
+    # Now run some unit tests that check some properties.
+    unit_tests(output_grid, output_gridsize)
 
 
 def run_tests():
@@ -167,6 +271,10 @@ def run_tests():
     test_multiple_input()
 
     print("")
+    print("Testing a randomly generated grid with known seed.")
+    test_random()
+
+    print("")
     print("=================================")
     print("All tests passed!")
     print("=================================")
@@ -175,3 +283,5 @@ def run_tests():
 if __name__ == '__main__':
 
     run_tests()
+
+    innocuous_variable = 1
